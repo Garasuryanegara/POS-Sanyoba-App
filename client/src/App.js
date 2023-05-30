726d411ec74ce766b262c1f5be2fc2c0a3e5d10e
Prekksu <novrialsandi@gmail.com>
2023-05-30 18:12:59 +0700

    s

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,24 @@
+import logo from "./logo.svg";
 import "./App.css";
-import { Routes } from "react-router-dom";
-import routes from "./routes/Routes";
-import { useEffect, useState } from "react";
-import { Center, Spinner } from "@chakra-ui/react";
 
 function App() {
-	const [isLoading, setIsLoading] = useState(true);
-
-	useEffect(() => {
-		new Promise((resolve) => {
-			setTimeout(() => {
-				resolve(setIsLoading(false));
-			}, 1000);
-		});
-	}, [isLoading]);
 	return (
-		<>
-			{isLoading ? (
-				<Center h={"100vh"}>
-					<Spinner />
-				</Center>
-			) : (
-				<Routes>{routes.map((val) => val)}</Routes>
-			)}
-		</>
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
 	);
 }
 
