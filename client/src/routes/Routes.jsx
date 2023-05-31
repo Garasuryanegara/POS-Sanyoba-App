890db8edd9ffed05b23aca1c7c9aaadbf9474586
Prekksu <novrialsandi@gmail.com>
2023-05-31 21:55:05 +0700

    component productpage

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,6 +1,12 @@
 import { Route } from "react-router-dom";
 import Homepage from "../pages/HomePage";
+import AddProductPage from "../pages/AddProductPage";
+import ProductPage from "../pages/ProductPage";
 
-const routes = [<Route path="/" element={<Homepage />}></Route>];
+const routes = [
+	<Route path="/" element={<Homepage />} />,
+	<Route path="/addProduct" element={<AddProductPage />} />,
+	<Route path="/product" element={<ProductPage />} />,
+];
 
 export default routes;
