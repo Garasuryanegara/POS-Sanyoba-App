7f20e0bc9079254041e3c8d0eec098de2ae3e372
Samdipro Nainggolan <71158606+samdipro@users.noreply.github.com>
2023-06-04 11:53:39 +0700

    rapiin admin landing page

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,6 +1,10 @@
 import { Route } from "react-router-dom";
 import Homepage from "../pages/HomePage";
+import AdminPage from "../pages/AdminLandingPage";
 
-const routes = [<Route path="/" element={<Homepage />}></Route>];
+const routes = [
+  <Route path="/" element={<Homepage />}></Route>,
+  <Route path="/admin" element={<AdminPage />}></Route>,
+];
 
 export default routes;
