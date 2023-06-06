e33bf7ff95dd4b4be8500f53ffb237bad5440905
Samdipro Nainggolan <71158606+samdipro@users.noreply.github.com>
2023-06-06 13:58:49 +0700

    Revert "Merge branch 'main' of https://github.com/samdipro/POS-Sanyoba-App"
    This reverts commit d84c9f1e2769e53198ff147e7144a06e9f5dd03a, reversing
    changes made to a2e5f8f8c5387923412ad350175a43f6b09d6a7d.

--- server/src/models/users.js
+++ server/src/models/users.js
@@ -1,40 +1,23 @@
 module.exports = (sequelize, Sequelize) => {
-
-  const user = sequelize.define("Users", {
-    name: Sequelize.STRING,
-    phone: Sequelize.STRING,
-    address: Sequelize.STRING,
-    password: Sequelize.STRING,
-    role: Sequelize.ENUM("admin", "cashier"),
-    active: {
-      type: Sequelize.BOOLEAN,
-      defaultValues: true,
+  const user = sequelize.define(
+    "Users",
+    {
+      name: Sequelize.STRING,
+      phone: Sequelize.STRING,
+      address: Sequelize.STRING,
+      password: Sequelize.STRING,
+      role: Sequelize.ENUM("admin", "user"),
+      active: {
+        type: Sequelize.BOOLEAN,
+        defaultValues: true,
+      },
+      avatar_blob: {
+        type: Sequelize.BLOB("long"),
+      },
     },
-    avatar_blob: {
-      type: Sequelize.BLOB("long"),
-    },
-  });
-
-//   const user = sequelize.define(
-//     "Users",
-//     {
-//       name: Sequelize.STRING,
-//       phone: Sequelize.STRING,
-//       address: Sequelize.STRING,
-//       password: Sequelize.STRING,
-//       role: Sequelize.ENUM("admin", "user"),
-//       active: {
-//         type: Sequelize.BOOLEAN,
-//         defaultValues: true,
-//       },
-//       avatar_blob: {
-//         type: Sequelize.BLOB("long"),
-//       },
-//     },
-//     {
-//       paranoid: true,
-//     }
-//   );
-
+    {
+      paranoid: true,
+    }
+  );
   return user;
 };
