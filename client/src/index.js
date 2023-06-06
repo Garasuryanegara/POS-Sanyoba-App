f53975732b7a50144102b34a9ecbabc7ce82dfa6
Samdipro Nainggolan <71158606+samdipro@users.noreply.github.com>
2023-06-06 14:05:04 +0700

    Revert "Revert "Merge branch 'main' of https://github.com/samdipro/POS-Sanyoba-App""
    This reverts commit 495793ab1dbd6fa1983243be07f78686d15ca4bc.

--- client/src/index.js
+++ client/src/index.js
@@ -5,16 +5,27 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./redux/auth";
+import rootReducer from "./redux/store";
+import thunk from "redux-thunk";
+const store = configureStore({ reducer: rootReducer, middleware: [thunk] });
+
 import "@fontsource/roboto";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-	<ChakraProvider>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</ChakraProvider>
+  <Provider store={store}>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </Provider>
 );
 
 reportWebVitals();
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers").userController;
 router.post("/", userController.register);
-router.get("/login", userController.login);
+router.post("/login", userController.login);
 router.get("/token", userController.getByToken, userController.getUserByToken);
 
 module.exports = router;
