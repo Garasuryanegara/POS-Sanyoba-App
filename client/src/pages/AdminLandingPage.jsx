a2e5f8f8c5387923412ad350175a43f6b09d6a7d
Samdipro Nainggolan <71158606+samdipro@users.noreply.github.com>
2023-06-06 13:42:16 +0700

    chart

--- client/src/pages/AdminLandingPage.jsx
+++ client/src/pages/AdminLandingPage.jsx
@@ -2,25 +2,20 @@ import { Container, Box, Center } from "@chakra-ui/react";
 import SidebarAdmin from "../components/SidebarAdmin";
 import MainAdmin from "../components/MainAdmin";
 export default function AdminPage() {
-  try {
-    return (
-      <Box backgroundColor={"#F1F1F1"} height={"100vh"} width={"100vw"}>
-        <Container maxW={"1440px"}>
-          <Box display={"flex"}>
-            <SidebarAdmin></SidebarAdmin>
-            <Box>
-              <MainAdmin></MainAdmin>
-            </Box>
+  return (
+    <Box
+      // height={"100vh"}
+      // width={"100vw"}
+      backgroundColor={"#F1F1F1"}
+    >
+      <Container maxW={"1440px"} padding={"0"}>
+        <Box display={"flex"}>
+          <SidebarAdmin></SidebarAdmin>
+          <Box width={"100%"}>
+            <MainAdmin></MainAdmin>
           </Box>
-        </Container>
-      </Box>
-    );
-  } catch (error) {
-    <>
-      <Center>
-        <h2>Error</h2>
-      </Center>
-      {console.log(error)}
-    </>;
-  }
+        </Box>
+      </Container>
+    </Box>
+  );
 }
