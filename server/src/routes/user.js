495793ab1dbd6fa1983243be07f78686d15ca4bc
Samdipro Nainggolan <71158606+samdipro@users.noreply.github.com>
2023-06-06 13:43:08 +0700

    Revert "Merge branch 'main' of https://github.com/samdipro/POS-Sanyoba-App"
    This reverts commit d84c9f1e2769e53198ff147e7144a06e9f5dd03a, reversing
    changes made to a2e5f8f8c5387923412ad350175a43f6b09d6a7d.

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers").userController;
 router.post("/", userController.register);
-router.post("/login", userController.login);
+router.get("/login", userController.login);
 router.get("/token", userController.getByToken, userController.getUserByToken);
 
 module.exports = router;
