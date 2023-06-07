051d4de45ad73b69e7f87a8a8af3c127d7e233d3
Rayyaki <rezky24.afrianata@gmail.com>
2023-06-07 11:50:24 +0700

    commit sprint 1

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -4,10 +4,36 @@ import AddProductPage from "../pages/AddProductPage";
 import ProductPage from "../pages/ProductPage";
 import AdminPage from "../pages/AdminLandingPage";
 import TransactionDetailPage from "../pages/TransactionDetailPage";
+import ProtectedPage from "./ProtectedPage";
+import LoginPage from "../pages/LoginPage";
 
 const routes = [
-  <Route path="/cashier" element={<Homepage />} />,
-  <Route path="/admin" element={<AdminPage />}></Route>,
+  <Route
+    path="/cashierLand"
+    element={
+      <ProtectedPage guestOnly={true} needLogin={true}>
+        <Homepage />
+      </ProtectedPage>
+    }
+  ></Route>,
+
+  <Route
+    path="/adminLand"
+    element={
+      <ProtectedPage guestOnly={true} needLogin={true}>
+        <AdminPage />
+      </ProtectedPage>
+    }
+  ></Route>,
+
+  <Route
+    path="/login"
+    element={
+      <ProtectedPage guestOnly={false} needLogin={false}>
+        <LoginPage />
+      </ProtectedPage>
+    }
+  ></Route>,
   <Route path="/addProduct" element={<AddProductPage />} />,
   <Route path="/product" element={<ProductPage />} />,
   <Route path="/trxDetail" element={<TransactionDetailPage />}></Route>,
